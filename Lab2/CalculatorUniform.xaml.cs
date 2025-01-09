e5bb0a290ab9198a633dcdeafac12f56a8410f8e
Alina Russkikh <alina.russkikh@metran.ru>
2025-01-09 17:46:50 +0500

    add lab 2

---
+++ Lab2/CalculatorUniform.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for CalculatorUniform.xaml
+    /// </summary>
+    public partial class CalculatorUniform : Window
+    {
+        public CalculatorUniform()
+        {
+            InitializeComponent();
+            enterUniformBlock.Text = 15075.8.ToString("N1");
+        }
+    }
+}
