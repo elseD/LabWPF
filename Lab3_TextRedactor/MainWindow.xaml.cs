9cb4db68c2a4f4390458d83a2524bc8c93b34454
Alina Russkikh <alina.russkikh@metran.ru>
2025-01-23 00:36:20 +0500

    Add event handlers to Lab3

--- Lab3_TextRedactor/MainWindow.xaml.cs
+++ Lab3_TextRedactor/MainWindow.xaml.cs
@@ -24,5 +24,81 @@ namespace Lab3_TextEditor
         {
             InitializeComponent();
         }
+
+        private void Button_Bold_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null && textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+                (sender as Button).BorderBrush = Brushes.DarkViolet;
+            }
+            else if(textBox != null)
+            {
+                textBox.FontWeight = FontWeights.Normal;
+                (sender as Button).BorderBrush = Brushes.Gray;
+            }
+        }
+
+        private void Button_Italic_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null && textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+                (sender as Button).BorderBrush = Brushes.DarkViolet;
+            }
+            else if (textBox != null)
+            {
+                textBox.FontStyle = FontStyles.Normal;
+                (sender as Button).BorderBrush = Brushes.Gray;
+            }
+        }
+
+        private void Button_Underline_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null && textBox.TextDecorations == null)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+                (sender as Button).BorderBrush = Brushes.DarkViolet;
+            }
+            else if(textBox != null)
+            {
+                textBox.TextDecorations = null;
+                (sender as Button).BorderBrush = Brushes.Gray;
+            }
+        }
+
+        private void RadioButtonBlack_Checked(object sender, RoutedEventArgs e)
+        {
+            if(textBox != null && (sender as RadioButton).IsChecked == true)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void RadioButtonRed_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null && (sender as RadioButton).IsChecked == true)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+
+        private void ComboBox_FontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                double size = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+                textBox.FontSize = size;
+            }
+        }
+
+        private void ComboBox_FontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                string family = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+                textBox.FontFamily = new FontFamily(family);
+            }
+        }
     }
 }
