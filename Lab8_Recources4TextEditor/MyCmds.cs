4ac4862f8fd85611603fe4f147eb1a98fd1bd0ec
Alina Russkikh <alina.russkikh@metran.ru>
2025-02-27 18:23:46 +0500

    add lab8

---
+++ Lab8_Recources4TextEditor/MyCmds.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab8_Recources4TextEditor
+{
+    internal class MyCmds
+    {
+        public static RoutedUICommand Exit { get; set; }
+
+        static MyCmds()
+        {
+            InputGestureCollection inputExit = new InputGestureCollection();
+            inputExit.Add(new KeyGesture(Key.T,ModifierKeys.Control, "Ctrl+T"));
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(MyCmds), inputExit);
+        }
+
+    }
+}
