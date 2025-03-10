17657227fa25daa8c1a16768a3d0924ecd302940
Alina Russkikh <alina.russkikh@metran.ru>
2025-03-10 21:29:30 +0500

    Add lab14

---
+++ Lab14_DataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14_DataTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Разработать шаблон данных для отображения списка товаров в компоненте ListBox.
+        // При помощи триггеров определите разный шаблон для товаров категории Еда и категории Бытовая техника.
+        public List<Product> productList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            productList = new List<Product>()
+            {
+                new Product()
+                {
+                    ProductName = "Ананас",
+                    Price = 250,
+                    ImgSource = "Img/pineapple.jpg",
+                    ProductCategory = CategoryList.Food
+                },
+                new Product()
+                {
+                    ProductName = "Блендер",
+                    Price = 2500,
+                    ImgSource = "Img/blender.jpg",
+                    ProductCategory = CategoryList.HouseholdAppliances
+                },
+                new Product()
+                {
+                    ProductName = "Варенье из клубники",
+                    Price = 210,
+                    ImgSource = "Img/jar.jpg",
+                    ProductCategory = CategoryList.Food
+                },
+                new Product()
+                {
+                    ProductName = "Гладильная система",
+                    Price = 1000,
+                    ImgSource = "Img/steamSystem.jpg",
+                    ProductCategory = CategoryList.HouseholdAppliances
+                },
+                new Product()
+                {
+                    ProductName = "Дубайский шоколад",
+                    Price=1490,
+                    ImgSource = "Img/choko.jpg",
+                    ProductCategory = CategoryList.Food
+                },
+            };
+
+            lstBox.ItemsSource = productList;
+        }
+    }
+}
---
+++ Lab14_DataTemplate/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14_DataTemplate
+{
+    public enum CategoryList { HouseholdAppliances, Food };
+
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public string ImgSource { get; set; }
+        public CategoryList ProductCategory { get; set; }
+    }
+}
